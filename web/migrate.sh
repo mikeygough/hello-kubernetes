fa68391638daf31f21fd17ddd6f7f2cdad9e6c68
mikey <mikey@hey.com>
2023-07-20 14:40:07 -0500

    add postgres to docker-compose

--- web/migrate.sh
+++ web/migrate.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-SUPERUSER_EMAIL = ${DJANGO_SUPERUSER_EMAIL:-"mikey@example.com"}
+SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"mikey@example.com"}
 cd /app/
 
 /opt/env/bin/python manage.py migrate --noinput
-
 /opt/env/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
